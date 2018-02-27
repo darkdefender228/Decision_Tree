7fcd8cf9275a63c4580c50ff3a474249b88aa8ee
Roman Ilechko <rooooman69@gmail.com>
2018-02-28 01:38:33 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -4,17 +4,18 @@
 
 #define MAX 81
 #define LEN 30
-#define COUNT_ROWS 10
+#define COUNT_CASES 2
+#define COUNT_FEATURE 3
 
 void read_csv(char* , char (*)[LEN]);
-double gini(double (*)[COUNT_ROWS][3], int, int);
+double gini(double (*)[COUNT_CASES][COUNT_FEATURE], int, int);
 int make_data(double (*)[3], char (*)[LEN]);
 double num_cases(int, int);
 int count(double*, double, int);
 
 int main(int argc, const char * argv[]) {
-    char data[COUNT_ROWS][LEN], path[] = "banknote.csv";
-    double converted_data[COUNT_ROWS][3];
+    char data[COUNT_CASES][LEN], path[] = "banknote.csv";
+    double converted_data[COUNT_CASES][COUNT_FEATURE];
     int len;
     
     
@@ -32,7 +33,7 @@ void read_csv(char* path, char data[][LEN]){
         strcpy(data[n], buf);
 }
 
-int make_data(double (*data)[3], char (*data_str)[LEN]){
+int make_data(double (*data)[COUNT_FEATURE], char (*data_str)[LEN]){
      double info[] = {2.771244718,1.784783929,0, 1.728571309,1.169761413,0,
         3.678319846,2.81281357,0, 3.961043357,2.61995032,0, 2.999208922,2.209014212,0,
         7.497545867,3.162953546,1, 9.00220326,3.339047188,1, 7.444542326,0.476683375,1,
@@ -46,14 +47,14 @@ int make_data(double (*data)[3], char (*data_str)[LEN]){
     return len;
 }
 
-double gini(double (*converted_data)[COUNT_ROWS][3], int len1, int len){
+double gini(double (*converted_data)[COUNT_CASES][COUNT_FEATURE], int len1, int len){
     double gini_index = 0.0, cases;
     double arr[len > len1 ? len : len1], p;
     int size[] = {len, len1}, T;
     
     cases = num_cases(len, len1);
     
-    for(int i = 0;i < 2; i++){
+    for(int i = 0;i < COUNT_CASES; i++){
         if(size[0] == 0 || size[1] == 0)
             continue;
         int estimation = 0.0;
@@ -65,7 +66,7 @@ double gini(double (*converted_data)[COUNT_ROWS][3], int len1, int len){
         for(int class = 0; class < 2; class++){
             for(int t = 0; t < T; t++)
                 arr[t] = converted_data[i][t][T - 1];
-            p = count(arr, class, T);
+            p = count(arr, class, T) / size[class];
             estimation += p * p;
         }
         gini_index += (1.0 - estimation) * (size[i] / cases);
